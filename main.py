d27eb56003513ee2cc77767434a32e50c82d856e
sean <pupjaa130@gmail.com>
2024-02-25 11:25:23 +0700

    #

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
 import pandas as pd
 
-df = pd.read_csv("power.csv")
+df = pd.read_csv("insurance.csv")
+print(df['sex'].unique())
 
-print(df.info())
+sex_map = {
+    'female':0,
+    'male':1
+}
+smoker_map = {
+    'no':0,
+    'yes':1
+}
+df['smoker']=df['smoker'].map(smoker_map)
+
+regian_encodes =pd.get_dummies(df, columns=['region'], dtype=int)
+df = pd.concat([df, regian_encodes],axis=1)
+df = df.drop(['region'],axis=1)
+
+print(df.head())
+
+df.to_csv('ready_insurance.csv',index=False)
